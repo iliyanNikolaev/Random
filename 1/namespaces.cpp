4e75766b1bb4476bc72f351b9d1f8a36d8f043e7
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-02-11 11:41:13 +0200

    namespaces

---
+++ 1/namespaces.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+namespace first {
+    int x = 1;
+}
+namespace second {
+    int x = 2;
+}
+
+int main() {
+    // using namespace first;
+    // using namespace second;
+    // int x = 0;
+    // std::string str = "testtext";
+    // std::cout << x;
+    // std::cout << first::x << '\n';
+    // std::cout << second::x << '\n';
+    using namespace std;
+    string str = "testtext";
+    cout << str;
+    return 0;
+}
