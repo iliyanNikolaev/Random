9ce81546ca1a8cf83a3a23947ff4b09abb624c9b
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-02-11 11:25:11 +0200

    variables and data types

---
+++ 1/variables-data-types.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    // integer (whole number)
+    int x = 5;
+    int y = 6;
+    int age = 26;
+    // std::cout << x << '\n';
+    // std::cout << y << '\n';
+
+    // double (number, including decimal)
+    double ten = 0.4;
+
+    //single character
+    char symbol = 'w';
+    char currency = '$';
+
+    //boolean
+    bool isExist = true;
+
+    //string
+    std::string name = "ilich";
+
+
+    std::cout << "Hello, i'm " << name << "\n";
+    std::cout << "I'm " << age << " y'o";
+
+    return 0;
+}
+
