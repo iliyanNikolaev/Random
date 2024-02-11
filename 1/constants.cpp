9ac1aca775a8babf24135e47b5d625824391f1d6
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-02-11 11:31:56 +0200

    constants

---
+++ 1/constants.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    const double PI = 3.14159;
+    double radius = 10;
+    double parameter = 2 * PI * radius;
+
+    std::cout << parameter << "cm";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+    
+    return 0;
+}
