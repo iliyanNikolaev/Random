5fd9ba3b0fa6a31d3bb5123018389da07b258e75
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-02-11 11:59:29 +0200

    type defs and aliases

---
+++ 1/type-defs-and-aliases.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
+
+int main() {
+    text_t firstName = "iliyan";
+    number_t age = 26;
+
+    std::cout << firstName << '\n';
+    std::cout << age;
+    return 0;
+}
