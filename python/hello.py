de88e88fc3d74d63f12a7e2030fc29a44940d564
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-11 00:34:11 +0200

    python conditional statements

---
+++ python/hello.py
@@ -0,0 +1,12 @@
+a = 6.5
+b = 6
+# print(type(a))
+
+# int, float, str, bool
+
+if a < b:
+    print('b is greater than a')
+elif b == 6:
+    print('b = 6')
+else:
+    print('a is greater than b')
