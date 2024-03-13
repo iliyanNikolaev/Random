1b730f2208080663ef69534dafb05abb00f66fb3
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-13 10:10:38 +0200

    for loop in python

---
+++ python/for.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+word = 'python'
+
+for char in word:
+    print(char)
+
+person = { 'name': 'Ilich', 'age': 26, 'city': 'Pleven' }
+
+for key, value in person.items():
+    print(f'{ key }: { value }')
