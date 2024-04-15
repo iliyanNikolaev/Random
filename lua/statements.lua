07fd5f4f71301afc7dbf7e35895f1139292e2294
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-04-15 13:37:36 +0300

    lua statements

---
+++ lua/statements.lua
@@ -0,0 +1,13 @@
+if true then
+    print('is always true')
+end
+
+local age = 26
+
+if age < 18 then
+    print('access denied')
+elseif age < 30 then
+    print('good')
+else
+    print('bad')
+end
